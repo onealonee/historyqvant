d660b80f71353ccb340a223644cb7864a9867771
NikolyaVOLCKOV <96441256+NikolyaVOLCKOV@users.noreply.github.com>
2024-07-20 13:22:47 +0300

    123123213

--- serve/index.js
+++ serve/index.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5001;
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+    user: 'volck',
+    host: '92.255.78.34',
+    database: 'hist_cvant',
+    password: 'volck',
+    port: 5432,
+});
+
+app.use(express.json());
+
+app.get('/figures', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT id, name, description FROM figures');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 // Маршрут для главной страницы
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+
 // Запуск сервера
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
-sequelize
-https://chatgpt.com/share/31ebd670-da82-4be6-b54b-2b73292e2f7d
+
